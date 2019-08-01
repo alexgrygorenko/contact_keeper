bebad905e8f7440f591517acd8ad868582fe4baa
Alex Grygorenko <alexey.grygorenko@gmail.com>
2019-08-01 10:28:01 +0300

    add contacts and contactItem components (hardcode)

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,62 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Sara Bla',
+        email: 'sara@gmail.com',
+        phone: '5672373723',
+        type: 'personal'
+      },
+      {
+        id: 2,
+        name: 'John Doe',
+        email: 'jDoe@gmail.com',
+        phone: '12365124324',
+        type: 'professional'
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add Contact
+
+  // Delete Contact
+
+  // Set current Contact
+
+  // Clear current Contact
+
+  // Update Contact
+
+  // Filter Contact
+
+  // Clear Filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
