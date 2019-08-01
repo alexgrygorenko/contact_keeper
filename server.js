361da6acd3d6722d044a0debbf68259d6902339a
Alex Grygorenko <alexey.grygorenko@gmail.com>
2019-08-01 09:09:46 +0300

    add navbar; router setup

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
