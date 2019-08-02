446d671d23a4908d9741a5e89bf455250ff68a42
Alex Grygorenko <alexey.grygorenko@gmail.com>
2019-08-02 13:02:26 +0300

    add logout logic

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -60,6 +60,7 @@ const AuthState = props => {
         type: REGISTER_SUCCESS,
         payload: res.data
       });
+
       loadUser();
     } catch (err) {
       dispatch({
@@ -69,11 +70,36 @@ const AuthState = props => {
     }
   };
   // Login User
-  const login = () => console.log('login');
+  const login = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
 
   // Logout
-  const logout = () => console.log('logout');
-
+  const logout = () => {
+    dispatch({
+      type: LOGOUT
+    });
+  };
   // Clear Errors
   const clearErrors = () =>
     dispatch({
